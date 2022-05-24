65249458f01591eaa09b6deffa0a92968af85d36
Joe Innes <joe@joeinn.es>
2022-05-25 00:05:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Double Prime Vowels
+Often, font designers neglect to create glyphs for the Hungarian long vowels. Generally, browsers will display ugly fallback alternatives to these glyphs. This package provides a solution for this problem.
+
+It's not a perfect solution, but it's a start.
+
+The double prime symbol, used to indicate inches normally, is used here to simulate an actual long acute accent.
+
+It won't look quite right, but hopefully it will look better than the ugly fallback, and it saves you finding a new font (although you should probably do that).
---
+++ index.js
@@ -0,0 +1,31 @@
+const styleTag = `<style>
+.hun, .hun-up {
+	position: relative;
+}
+
+.hun:before {
+	content: '″';
+	position: absolute;
+	top: -0.1em;
+	left: 50%;
+  transform: translateX(-45%);
+}
+
+.hun-up:before {
+	content: '″';
+	position: absolute;
+	top: -0.3em;
+	left: 50%;
+  transform: translateX(-45%);
+}
+</style>
+`;
+
+let rawText = document.body.innerHTML;
+rawText = rawText.replaceAll("ő", '<span class="hun">o</span>');
+rawText = rawText.replaceAll("ű", '<span class="hun">u</span>');
+rawText = rawText.replaceAll("Ő", '<span class="hun-up">O</span>');
+rawText = rawText.replaceAll("Ű", '<span class="hun-up">U</span>');
+
+document.body.innerHTML = rawText;
+document.body.appendChild(document.createElement("style")).outerHTML = styleTag;
