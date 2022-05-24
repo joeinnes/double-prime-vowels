2128812d4e86bc46b282339334fca222b6d7c1b2
Joe Innes <joe@joeinn.es>
2022-05-25 00:16:25 +0200

    Move to a function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const styleTag = `<style>
+function doublePrimeVowels(options) {
+  const { lowerCaseOffset = -0.1, upperCaseOffset = -0.3 } = options;
+
+  const styleTag = `<style>
 .hun, .hun-up {
 	position: relative;
 }
@@ -6,7 +9,7 @@ const styleTag = `<style>
 .hun:before {
 	content: '″';
 	position: absolute;
-	top: -0.1em;
+	top: ${lowerCaseOffset}em;
 	left: 50%;
   transform: translateX(-45%);
 }
@@ -14,18 +17,22 @@ const styleTag = `<style>
 .hun-up:before {
 	content: '″';
 	position: absolute;
-	top: -0.3em;
+	top: ${upperCaseOffset}em;
 	left: 50%;
   transform: translateX(-45%);
 }
 </style>
 `;
 
-let rawText = document.body.innerHTML;
-rawText = rawText.replaceAll("ő", '<span class="hun">o</span>');
-rawText = rawText.replaceAll("ű", '<span class="hun">u</span>');
-rawText = rawText.replaceAll("Ő", '<span class="hun-up">O</span>');
-rawText = rawText.replaceAll("Ű", '<span class="hun-up">U</span>');
+  let rawText = document.body.innerHTML;
+  rawText = rawText.replaceAll("ő", '<span class="hun">o</span>');
+  rawText = rawText.replaceAll("ű", '<span class="hun">u</span>');
+  rawText = rawText.replaceAll("Ő", '<span class="hun-up">O</span>');
+  rawText = rawText.replaceAll("Ű", '<span class="hun-up">U</span>');
 
-document.body.innerHTML = rawText;
-document.body.appendChild(document.createElement("style")).outerHTML = styleTag;
+  document.body.innerHTML = rawText;
+  document.body.appendChild(document.createElement("style")).outerHTML =
+    styleTag;
+}
+
+export default doublePrimeVowels;
